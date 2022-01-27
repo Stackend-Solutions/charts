b8006dd4f47553821db7d614c946e2cf6528d36a
mazurbeam <mazurbeam@gmail.com>
2022-01-27 06:41:27 -0800

    added upload.sh and update readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # charts
 Helm chart templates
+
+
+### Environment
+To use upload script, create a `.env` in root directory
+
+```bash
+CHARTMUSEUM_USER=<username>
+CHARTMUSEUM_PASS=<password>
+```
+
+Then run in terminal
+```bash
+helm package ./template_directory
+
+./scripts/upload.sh ./template_directory/***.tgz
+```
---
+++ scripts/upload.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# takes 1 args
+# 1 - directory/filename.tgz - created by `helm package .` command in directory
+
+cd "$(dirname "$0")/.." || exit
+
+if [ -f .env ]
+then
+  export $(cat .env | xargs)
+else
+  echo ".env file not set"
+  exit 0
+fi
+
+fspec="$1"
+filename="${fspec##*/}"  # get filename
+dirname="${fspec%/*}" # get directory/path name
+
+cd "$dirname"
+
+curl -u "$CHARTMUSEUM_USER":"$CHARTMUSEUM_PASS" --data-binary "@$filename" https://chartmuseum.stackendsolutions.com/api/charts
